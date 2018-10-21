90cddcdda89b09b8395fa828b6fba23febe7228e
Andrew Kim <kima1995@uw.edu>
2018-10-21 11:26:54 -0700

    Push Notification enabled

--- app/src/main/java/com/example/andykim/hackathonapp/LoginActivity.java
+++ app/src/main/java/com/example/andykim/hackathonapp/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -17,6 +18,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 
 public class LoginActivity extends AppCompatActivity {
